679169cafdba2c88dae4da701b8e4dac26de33c1
SumiyaMim <sumiyaislam1093@gmail.com>
2025-02-10 21:29:26 +0600

    create hero to footer components

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+// Dropdown
 const mobile_bars = document.querySelector(".mobile_bars");
 mobile_bars.addEventListener("click", () => {
   document.querySelector(".main_menu").classList.toggle("open");
 });
+
+// Current Year
+document.getElementById("year").innerHTML = new Date().getFullYear();
